9289bd310af7e93403096a36be906bb38a20059f
MaicolRoldan <142542052+MaicolRoldan@users.noreply.github.com>
2023-10-06 14:56:20 -0500

    Add files via upload

--- Crud/js/index.js
+++ Crud/js/index.js
@@ -1,11 +1,10 @@
 import { CRUD } from "./CRUD.js";
 
 function app(){
-	let crud = new CRUD("ejemplo");
-    crud.create([1, 2, 3]);
-    crud.create({ message: "hola mundo"});
+	let nombre = new CRUD("ejemplo");
+    nombre.create(["maicol", "andres", "roldan"]);
 
     let crud2 = new CRUD("ejemplo");
-    console.log(crud2.readAll());
+    console.log(nombre.readAll());
 }
 app();
